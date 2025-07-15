d116d327b2c52f8ded7436aa651bff8f89a2077a
Raniya <114820375+Raniyaaaa@users.noreply.github.com>
2025-07-15 13:10:05 +0530

    Update otpRoutes.js

--- backend/routes/otpRoutes.js
+++ backend/routes/otpRoutes.js
@@ -4,7 +4,7 @@ const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/send-otp', protect, sendOtp);        // Patient triggers
-router.post('/verify-otp', verifyOtp);             // Doctor uses OTP
+router.post('/send-otp', protect, sendOtp);       
+router.post('/verify-otp', verifyOtp);             
 
 module.exports = router;
